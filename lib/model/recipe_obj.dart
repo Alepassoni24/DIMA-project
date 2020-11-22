9592f8fcb7ada4e43076be351bb4fffdfcdae5cb
Camillucci <nicola.camillucci@mail.polimi.it>
2020-11-22 18:12:25 +0100

    Added recipes queries with mock data

---
+++ lib/model/recipe_obj.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class RecipeObj {
+  final String recipeId;
+
+  RecipeObj({this.recipeId});
+}
+
+class RecipeData {
+  final String recipeId;
+
+  final String title;
+  final String subtitle;
+  final String description;
+  final String imageURL;
+  final int rating;
+  final int timeInMinutes;
+  final Timestamp submissionTime;
+  
+  RecipeData({this.recipeId, this.title, this.subtitle, this.description, this.imageURL, this.rating, this.submissionTime, this.timeInMinutes});
+}
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dima_project/model/recipe_obj.dart';
 import 'package:dima_project/model/user_obj.dart';
 
 class DatabaseService {
@@ -8,6 +9,8 @@ class DatabaseService {
   //collection reference
   final CollectionReference userCollection =
       FirebaseFirestore.instance.collection('user');
+  final CollectionReference recipeCollection =
+    FirebaseFirestore.instance.collection('recipe');
 
   Future updateUserData(String username) async {
     return await userCollection.doc(uid).set({
@@ -31,5 +34,34 @@ class DatabaseService {
   //get user doc stream
   Stream<UserData> get userData {
     return userCollection.doc(uid).snapshots().map(_userDataFromSnapshot);
+  }
+
+  //get recipe stream
+  Stream<QuerySnapshot> get recipes {
+    return recipeCollection.snapshots();
+  }
+
+  //get recipe data from snapshot
+  RecipeData recipeDataFromSnapshot(DocumentSnapshot documentSnapshot) {
+    return RecipeData(
+      recipeId: documentSnapshot.id,
+      title: documentSnapshot['title'],
+      subtitle: documentSnapshot['subtitle'],
+      description: documentSnapshot['description'],
+      imageURL: documentSnapshot['imageURL'],
+      rating: documentSnapshot['rating'],
+      timeInMinutes: documentSnapshot['timeInMinutes'],
+      submissionTime: documentSnapshot['submissionTime'],
+    );
+  }
+
+  //get recipe doc stream
+  Stream<RecipeData> recipeData(String recipeId) {
+    return userCollection.doc(recipeId).snapshots().map(recipeDataFromSnapshot);
+  }
+
+  //get stream of last num recipes
+  Stream<QuerySnapshot> getLastRecipes(int num) {
+    return recipeCollection.orderBy('submissionTime', descending: true).limit(num).snapshots();
   }
 }
